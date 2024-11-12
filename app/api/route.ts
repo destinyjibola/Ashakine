62af00a057efe74add397d345c203b082938bfb1
destinyjibola <destinyjibola@gmail.com>
2024-11-12 08:06:52 +0100

    testing api

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,15 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
+import User from "../../model/user";
+import { connect } from "../../lib/dbConfig";
+
+connect();
 
 export async function POST(request: NextRequest) {
   try {
-    const reqBody = await request.json()
-    const { name, email, password } = reqBody
-    const newuser = await prisma.user.create({
-      data:{
-          name, email, password
-      }
-    })
+    const reqBody = await request.json();
+    const { name, email, password } = reqBody;
+
+    const newuser = new User({
+      name,
+      email,
+      password,
+    });
+    // const newuser = await prisma.user.create({
+    //   data: {
+    //     name,
+    //     email,
+    //     password,
+    //   },
+    // });
     return NextResponse.json(
       {
         message: "Data added succesfully",
