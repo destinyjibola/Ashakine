b17c1dbdbb0dcbd819554e154ae93d0aa09310c0
mythroscleaners <mythross25@gmail.com>
2025-08-11 16:40:18 +0100

    admin adjustment

--- components/auth/Header.tsx
+++ components/auth/Header.tsx
@@ -13,7 +13,7 @@ interface HeaderProps {
 const Header = ({ label }: HeaderProps) => {
   return (
     <div className="w-full flex flex-col gap-y-4 items-center">
-        <h1 className={cn("text-3xl font-semibold",font.className)}>Asherkine</h1>
+        <h1 className={cn("text-3xl font-semibold",font.className)}>Spinly</h1>
         <p className="text-muted-foreground text-sm">{label}</p>
     </div>
   );
